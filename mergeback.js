c4f63c6508f286d534443d3da47f6a09dc3fa692
John Simon <johns@smartek21.com>
2018-06-30 22:27:40 -0700

    Fixing bug in generating authentication string

--- mergeback.js
+++ mergeback.js
@@ -1,41 +1,83 @@
-// @ts-check
-
-const axios = require("axios");
+const axios = require('axios');
 require('dotenv').config();
-const { USER, PASS, ORGANIZATION, REPO } = process.env;
+const { USERNAME, PASS, ORGANIZATION, REPO } = process.env;
 
 
-console.log()
-const url = "https://api.github.com/repos/johns-smartek/node/pulls";
-const getPulls = async (githubRepositoryBase, authorizationHash) => {
-  const params = {
-    method: 'get',
-    url: `${githubRepositoryBase}/pulls`,
-    authorization: authorizationHash,
-  };
-
+const doWebCall = async (params) => {
   try {
     const result = await axios(params);
     return result.data;
   } catch (error) {
-    console.log(error);
+    return error;
   }
+}
+
+const getPulls = async (githubRepositoryBase, authorizationHash) => {
+  const params = {
+    method: 'get',
+    url: `${githubRepositoryBase}/pulls`,
+    headers:{
+      Authorization: authorizationHash,
+      "Content-Type": "application/json"
+    },
+  };
+  return await doWebCall(params);
 };
 
-const getBasicAuthenticationHash = (user, pass) => Buffer.from(`Basic ${user}:${pass}`).toString('base64');
-const buildGitHubAPIBaseUrl = (organization, repo) => `https://api.github.com/repos/${organization}/${repo}/pulls`;
+const createPullRequests = async (githubRepositoryBase, authorizationHash, pullRequestParameters) => {
+  const params = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: authorizationHash,
+    },
+    url: `${githubRepositoryBase}/pulls`,
+    data: pullRequestParameters 
+  };
+  return await doWebCall(params);
+};
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+/*
+{
+        "title": "My Pull Request",
+        "body": "Please review and merge",
+        "head": "canary-base",
+        "base": "master"	
+    }
+*/
+
+const getBasicAuthenticationHash = (user, pass) =>{
+    const base64UserPass = Buffer.from(`${user}:${pass}`).toString('base64');
+    return `Basic ${base64UserPass}`
+  };
+const buildGitHubAPIBaseUrl = (organization, repo) => `https://api.github.com/repos/${organization}/${repo}`;
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 async function demo() {
   const result = await getPulls(
     buildGitHubAPIBaseUrl(ORGANIZATION, REPO),
-    getBasicAuthenticationHash(USER, PASS));
+    getBasicAuthenticationHash(USERNAME, PASS));
 
-    await sleep(5000)
-    console.log(result);
+  await sleep(5000)
+  console.log(result);
 }
 
-demo();
+async function demo2() {
+  const result = await createPullRequests(
+    buildGitHubAPIBaseUrl(ORGANIZATION, REPO),
+    getBasicAuthenticationHash(USERNAME, PASS),
+    {
+      "title": "My Pull Request",
+      "body": "Please review and merge``` text here \n more text```",
+      "head": "canary-base",
+      "base": "master"
+    }
+  );
+
+  await sleep(5000)
+  console.log(result);
+}
+
+
+demo2();
